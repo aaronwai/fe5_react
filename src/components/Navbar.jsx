b7310259c7cffd72c7a431086e082911a24ad0ee
aaronwai <aaron.lung.wai@gmail.com>
2026-03-10 10:28:50 +0800

    react done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import SocialLinks from "./SocialLinks";
 import PageLinks from "./PageLinks";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 const Navbar = () => {
+  const [isToggled, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!isToggled);
+  };
   return (
     <nav className='navbar'>
       <div className='container navbar-flex'>
-        <a href='./index.html' className='logo'>
+        <a href='#home' className='logo'>
           <img src={logo} alt='' />
         </a>
         <div className='main-menu'>
@@ -19,11 +24,11 @@ const Navbar = () => {
         </div>
 
         <div className='mobile-menu'>
-          <div className='mobile-menu-toggle'>
+          <div className='mobile-menu-toggle' onClick={handleToggle}>
             <i className='fa-solid fa-bars'></i>
           </div>
           <div className='mobile-menu-items'>
-            <ul className='mobile-menu-list'>
+            <ul className={`mobile-menu-list ${isToggled ? "active" : ""}`}>
               <PageLinks />
             </ul>
           </div>
