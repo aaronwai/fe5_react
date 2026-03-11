cbb1e619aa1a1006499e893092dd49391d9f2a54
aaronwai <aaron.lung.wai@gmail.com>
2026-03-11 09:48:11 +0800

    update setting for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fe5_react/",
+  // a. turn on next line for github page deploy
+  // base: "/fe5_react/",
+  // b. turn on next line for netlify deploy
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
